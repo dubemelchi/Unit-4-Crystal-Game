158fb147c0942ef485347c373408c8dee461736a
dubemelchi <dubemelchi@gmail.com>
2019-06-03 20:44:03 -0400

    JS for random number generator on "start game"

--- Assets/JavaScript/Game.js
+++ Assets/JavaScript/Game.js
@@ -1,14 +1,7 @@
 //jquery for random number generator
 
-$("#startGame").on("click", function() {
 
-      var random = Math.floor(Math.random() * 1000) + 1;
-
-        // ... and then dump the random number into our random-number div.
-        $("#random-number").text(random);
-
-
-          
-    
-      })
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
   
